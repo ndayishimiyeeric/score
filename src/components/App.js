5e6d74d2ba1d72858e079cefd0897ecbf52a9815
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-06-21 16:06:09 +0200

    Provide store and Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Routes, Route } from 'react-router-dom';
+import { getData } from '../redux/sportsSlice/sportsSlice';
+import Sports from './Sports';
+import Details from './Details';
+
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getData());
+  }, []);
+
   return (
-    <div className="App">
-      <h3>Hello</h3>
+    <div>
+      <Routes>
+        <Route path="/" element={<Sports />} />
+        <Route path="/detail" element={<Details />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import './components/styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +10,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
 
